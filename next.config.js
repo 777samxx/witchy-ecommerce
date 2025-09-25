506103d51af0a11d180232810a6e61502927a85c
777samxx <caseyashelsa@gmail.com>
2025-09-25 19:00:54 +0100

    Create next.config.js file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['littleladywitching.co.uk', 'res.cloudinary.com'],
+  },
+};
+
+module.exports = nextConfig;
